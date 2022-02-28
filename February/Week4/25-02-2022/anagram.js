96f2a37a9792a2b3bd6a24ddbb976dfb1332af95
Avantika-Mishra <96308161+Avantika-Mishra@users.noreply.github.com>
2022-02-28 19:35:37 +0530

    Update anagram.js

--- February/Week4/25-02-2022/anagram.js
+++ February/Week4/25-02-2022/anagram.js
@@ -2,16 +2,16 @@ function checkStringsAnagram(a,b){
     let len1 =a.length;
     let len2 =b.length;
     if(len1 !== len2){
-        console.log('Invalid input');
-        return
+        document.write('Invalid input');
+        return;
     }
     let str1 = a.split('').sort().join('');
     let str2 = b.split('').sort().join('');
     if(str1 === str2){
-        console.log("True");
+        document.write("True");
     }
     else{
-        console.log("False");
+        document.write("False");
     }
 }
 checkStringsAnagram("fried","fired")
