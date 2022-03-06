9a4b355420d9c860f19ae4a76c7a8e5fa6df9d9b
Avantika-Mishra <96308161+Avantika-Mishra@users.noreply.github.com>
2022-03-06 12:22:19 +0530

    Add files via upload

---
+++ March/Week1/02-03-2022/Gallery.js
@@ -0,0 +1,171 @@
+//For first pic
+var counter = 0;
+function render(){
+    var disp = document.getElementById('op_text1');
+    disp.innerText = counter;
+}
+var counter=0;
+function like(){
+    counter += 1;
+    render();
+}
+function dislike(){
+    counter -= 1;
+    render();
+}
+function render(){
+    var disp = document.getElementById('op_text1');
+    disp.innerText = counter;
+}
+//For second pic
+var count = 0;
+function renders(){
+    var disp = document.getElementById('counting');
+    disp.innerText = count;
+}
+var count=0;
+function increment(){
+    count += 1;
+    renders();
+}
+function decrement(){
+    count -= 1;
+    renders();
+}
+function renders(){
+    var disp = document.getElementById('counting');
+    disp.innerText = count;
+}
+//For third pic
+var total = 0;
+function rend(){
+    var disp = document.getElementById('op_texts3');
+    disp.innerText = total;
+}
+var total=0;
+function likes(){
+    total += 1;
+    rend();
+}
+function dislikes(){
+    total -= 1;
+    rend();
+}
+function rend(){
+    var disp = document.getElementById('op_texts3');
+    disp.innerText = total;
+}
+//For forth pic
+var num = 0;
+function ren(){
+    var disp = document.getElementById('ops_text');
+    disp.innerText = num;
+}
+var num=0;
+function liked(){
+    num += 1;
+    ren();
+}
+function disliked(){
+    num -= 1;
+    ren();
+}
+function ren(){
+    var disp = document.getElementById('ops_text');
+    disp.innerText = num ;
+}
+//For fifth pic
+var num5 = 0;
+function ren5(){
+    var disp = document.getElementById('ops_text5');
+    disp.innerText = num5;
+}
+var num5=0;
+function like5(){
+    num5 += 1;
+    ren5();
+}
+function dislike5(){
+    num5 -= 1;
+    ren5();
+}
+function ren5(){
+    var disp = document.getElementById('ops_text5');
+    disp.innerText = num5 ;
+}
+//For sixth pic
+var num6 = 0;
+function ren6(){
+    var disp = document.getElementById('ops_text6');
+    disp.innerText = num6;
+}
+var num6=0;
+function like6(){
+    num6 += 1;
+    ren6();
+}
+function dislike6(){
+    num6 -= 1;
+    ren6();
+}
+function ren6(){
+    var disp = document.getElementById('ops_text6');
+    disp.innerText = num6 ;
+}
+//For seventh pic
+var num7 = 0;
+function ren7(){
+    var disp = document.getElementById('ops_text7');
+    disp.innerText = num7;
+}
+var num7 = 0;
+function like7(){
+    num7 += 1;
+    ren7();
+}
+function dislike7(){
+    num7 -= 1;
+    ren7();
+}
+function ren7(){
+    var disp = document.getElementById('ops_text7');
+    disp.innerText = num7 ;
+}
+//For eighth pic
+var num8 = 0;
+function ren8(){
+    var disp = document.getElementById('ops_text8');
+    disp.innerText = num8;
+}
+var num8 = 0;
+function like8(){
+    num8 += 1;
+    ren8();
+}
+function dislike8(){
+    num8 -= 1;
+    ren8();
+}
+function ren8(){
+    var disp = document.getElementById('ops_text8');
+    disp.innerText = num8 ;
+}
+//For ninth pic
+var num9 = 0;
+function ren9(){
+    var disp = document.getElementById('ops_text9');
+    disp.innerText = num9;
+}
+var num9 = 0;
+function like9(){
+    num9 += 1;
+    ren9();
+}
+function dislike9(){
+    num9 -= 1;
+    ren9();
+}
+function ren9(){
+    var disp = document.getElementById('ops_text9');
+    disp.innerText = num9 ;
+}
